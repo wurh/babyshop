98750f3c932e8f4a57bc91d42241b020e1337d98
吴荣华 <wurh@ucweb.com>
2014-09-23 21:55:45 +0800

    [UPD] 更新静态资源路径

--- components/pages/index/index.js
+++ components/pages/index/index.js
@@ -6,77 +6,77 @@ module.exports = {
             {
                 title: '模块化开发',
                 icon: 'cubes',
-                href: '/#!/modular'
+                href: '/babyshop/#!/modular'
             },
             {
                 title: '按版本发布',
                 icon: 'code-fork',
-                href: '/#!/settings?title=version'
+                href: '/babyshop/#!/settings?title=version'
             },
             {
                 title: '模块生态',
                 icon: 'github',
-                href: '/#!/components'
+                href: '/babyshop/#!/components'
             },
             {
                 title: '按需加载',
                 icon: 'crosshairs',
-                href: '/#!/framework?title=scrat.js'
+                href: '/babyshop/#!/framework?title=scrat.js'
             },
             {
                 title: '请求合并',
                 icon: 'flash',
-                href: '/#!/framework?title=3. 网络请求'
+                href: '/babyshop/#!/framework?title=3. 网络请求'
             },
             {
                 title: '本地缓存',
                 icon: 'database',
-                href: '/#!/framework?title=4. 本地缓存'
+                href: '/babyshop/#!/framework?title=4. 本地缓存'
             },
             {
                 title: '代码压缩',
                 icon: 'leaf',
-                href: '/#!/settings?title=settings.optimizer.uglify-js'
+                href: '/babyshop/#!/settings?title=settings.optimizer.uglify-js'
             },
             {
                 title: '代码校验',
                 icon: 'search',
-                href: '/#!/settings?title=settings.lint.jshint'
+                href: '/babyshop/#!/settings?title=settings.lint.jshint'
             },
             {
                 title: 'CSS雪碧图',
                 icon: 'th',
-                href: '/#!/settings?title=settings.spriter.csssprites'
+                href: '/babyshop/#!/settings?title=settings.spriter.csssprites'
             },
             {
                 title: '本地服务器',
                 icon: 'rocket',
-                href: '/#!/command?title=scrat server start'
+                href: '/babyshop/#!/command?title=scrat server start'
             },
             {
                 title: '文件监听',
                 icon: 'eye',
-                href: '/#!/command?title=本地开发'
+                href: '/babyshop/#!/command?title=本地开发'
             },
             {
                 title: '自动刷新',
                 icon: 'refresh',
-                href: '/#!/command?title=本地开发'
+                href: '/babyshop/#!/command?title=本地开发'
             },
             {
                 title: '资源内嵌',
                 icon: 'archive',
-                href: '/#!/todo'
+                href: '/babyshop/#!/todo'
             },
             {
                 title: '多语言编译',
                 icon: 'code',
-                href: '/#!/settings?title=settings.parser.stylus'
+                href: '/babyshop/#!/settings?title=settings.parser.stylus'
             },
             {
                 title: '项目脚手架',
                 icon: 'wrench',
-                href: '/#!/command?title=scrat init'
+                href: '/babyshop/#!/command?title=scrat init'
             }
         ];
         var tpl = __inline('index.handlebars');
